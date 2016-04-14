10220543c8ec5e9e0830fe9d182b3aaa920c5e1c
diego2013 <diego@mdpnp.org>
2016-04-14 13:59:44 -0400

    corrected order of imports

--- client/main.js
+++ client/main.js
@@ -1,8 +1,9 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+
 import '../imports/ui/body.js';
+import './main.html';
 
 // Template.hello.onCreated(function helloOnCreated() {
 //   // counter starts at 0
