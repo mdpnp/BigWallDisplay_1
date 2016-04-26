cb8a854624be56bd264846db79c169416a8c821a
diego2013 <diego@mdpnp.org>
2016-04-26 10:08:42 -0400

    added deny rules to explicitly block any writes to the database

--- imports/api/patients.js
+++ imports/api/patients.js
@@ -1,8 +1,28 @@
 import { Mongo } from 'meteor/mongo';
  
+// patient_demographic is a collections with basic patient info 
 export const Patients = new Mongo.Collection('patient_demographic');
 
+
+// Explicitly deny any writes in the patient_demographic collection. 
+// This application is a "read only" app to display info, not to modify data
+Patients.deny({
+	insert : function(userid, doc){
+		return true;
+	}
+
+	,update : function(userId, doc, fields, modifier){
+		return true;
+	}
+
+	,remove : function(userId, doc){
+		return true;
+	}
+})
+
 if (Meteor.isServer) {
+
+// Publication	
   Meteor.publish('patients_demo', function patientsPublication() {
     return Patients.find();
   });
