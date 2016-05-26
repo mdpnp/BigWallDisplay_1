cfb72b1c8893e461ae7eaea91db1de34fda99459
diego2013 <diego@mdpnp.org>
2016-05-26 11:59:06 -0400

    codified BP logic

--- imports/ui/patient_info_ui.js
+++ imports/ui/patient_info_ui.js
@@ -59,15 +59,54 @@ receives a JSON object with patient data (demographics, data metrics)
 Returns a JSON object to display the patient assesment with a certain layout
 */
 export const patient_assesment = function(patientdata){
+
+	//properties from te patient assesment
 	let temp_avg = patientdata === undefined || patientdata.temperature === undefined ? '-' : patientdata.temperature.sum / patientdata.temperature.count;
-	// console.log(temp_avg)
+	let bp_sys_avg = patientdata === undefined || patientdata.blood_pressure_sys === undefined ? '-' : patientdata.blood_pressure_sys.sum / patientdata.blood_pressure_sys.count;
+    let bp_dias_avg = patientdata === undefined || patientdata.blood_pressure_dias === undefined ? '-' : patientdata.blood_pressure_dias.sum / patientdata.blood_pressure_dias.count;
+    let hr_avg = patientdata === undefined || patientdata.heart_rate === undefined ? '-' : patientdata.heart_rate.sum / patientdata.heart_rate.count;
+    let spo2_avg = patientdata === undefined || patientdata.spo2 === undefined ? '-' : patientdata.spo2.sum / patientdata.spo2.count;
+    let pr_avg = patientdata === undefined || patientdata.pulse_rate === undefined ? '-' : patientdata.pulse_rate.sum / patientdata.pulse_rate.count;
+
+    let assesment = healthy; //basic assesment
+    let message = ''; //message to be displayed with the assesment
+
+    //BLOOD PRESSURE
+
+    /* 
+    Systolic in mmHg    Diastolic in mmHg  Category
+    below 120				below 80		Normal blood pressure
+    120 - 139				80 - 39			Prehyertension
+    140 - 159				90 - 99			Stage 1 hypertension
+    160 or higher			100 or higher   Stage 2 Hyperension
+
+    */
+    if(bp_sys_avg  <= 120  && bp_dias_avg <= 80 ){
+		message += " Normal Blood Pressure ";
+    }else if( (bp_sys_avg  >= 120 && bp_sys_avg  <= 139) && 
+    	(bp_dias_avg >= 80  && bp_dias_avg <= 89 )){
+    	//prehypertension
+	    // assesment = alert;
+	    message += " Prehypertension ";
+	} else if( (bp_sys_avg  >= 140 && bp_sys_avg  <= 159) && 
+    		   (bp_dias_avg >= 90  && bp_dias_avg <= 99 )){
+    	//Stage 1 prehypertension
+	    assesment = alert;
+	    message += " Stage 1 Hypertension ";
+	} else if(bp_sys_avg  >= 160  && bp_dias_avg >= 100 ){
+    	//prehypertension
+	    assesment = danger;
+	    message += " Stage 2 Hypertension ";
+	}
+
+
+
 	if(temp_avg === '-'){
 		return healthy
 	}
 	else{
-		let assesment = healthy;
+		
 		temp_avg = Number(temp_avg);
-		console.log(temp_avg)
 		if(temp_avg <= 36.1 || temp_avg >= 37.8){
 			assesment = alert;
 			assesment.message = "Check temperature"
